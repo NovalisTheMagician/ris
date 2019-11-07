63e8ee7bc7e072720a7a0b8d388f000af2f8b4c0
Patrik Körn <patrik.koern@gmail.com>
2019-11-07 16:51:05 +0100

    added more diagnostic output

--- SConstruct
+++ SConstruct
@@ -2,4 +2,4 @@ src_files = Glob('src/*.cpp')
 
 src_objs = Object(src_files, CPPPATH=['src', 'libs/glm/include', 'libs/gli/include', 'libs/glbinding/include', 'libs/glfw/include'], CXXFLAGS='/EHsc')
 
-game = Program('RIS', src_objs, LIBS=['glbinding', 'glbinding-aux', 'glfw3dll', 'user32', 'opengl32'], LIBPATH=['libs/glbinding/lib', 'libs/glfw/lib'])
+game = Program('RIS', src_objs, LIBS=['user32', 'kernel32', 'gdi32', 'opengl32', 'glbinding', 'glbinding-aux', 'glfw3dll'], LIBPATH=['libs/glbinding/lib', 'libs/glfw/lib'])
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,10 @@
 #define UNICODE
 #include <Windows.h>
 
-#include <glbinding/gl45core/gl.h>
+#include <glbinding/gl46core/gl.h>
 #include <glbinding/glbinding.h>
 
+//#define GLFW_DLL
 #include <GLFW/glfw3.h>
 
 #include <string>
@@ -17,15 +18,17 @@ using namespace gl;
 
 int main(int argc, char *argv[])
 {
+    //FreeConsole();
+
     GLFWwindow* window;
 
     /* Initialize the library */
     if (!glfwInit())
         return -1;
 
-    glfwWindowHint(GLFW_SRGB_CAPABLE, GLFW_TRUE);
+    glfwWindowHint(GLFW_SRGB_CAPABLE, true);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 
     /* Create a windowed mode window and its OpenGL context */
     window = glfwCreateWindow(800, 600, "Hello World", NULL, NULL);
@@ -41,21 +44,30 @@ int main(int argc, char *argv[])
     glbinding::initialize(glfwGetProcAddress);
 
     string version = reinterpret_cast<const char*>(glGetString(GL_VERSION));
+    string vendor = reinterpret_cast<const char*>(glGetString(GL_VENDOR));
+    string renderer = reinterpret_cast<const char*>(glGetString(GL_RENDERER));
+    string shaderVer = reinterpret_cast<const char*>(glGetString(GL_SHADING_LANGUAGE_VERSION));
     cout << version << endl;
+    cout << renderer << endl;
+    cout << shaderVer << endl;
+    cout << vendor << endl;
+
+    glEnable(GL_FRAMEBUFFER_SRGB);
+    //glEnable(GL_TEXTURE);
 
     gl::glClearColor(0.392f, 0.584f, 0.929f, 1.0f);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
+        /* Poll for and process events */
+        glfwPollEvents();
+
         /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
     }
 
     glfwTerminate();

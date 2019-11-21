3d014a85e2831bf25755d0dc2af82a00d66f623d
Patrik Körn <patrik.koern@gmail.com>
2019-11-21 20:00:46 +0100

    added input interface

---
+++ src/common/IInput.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+namespace RIS
+{
+    enum class InputAction
+    {
+        //Movement
+        MOVE_FORWARD,
+        MOVE_BACKWARD,
+        MOVE_LEFT,
+        MOVE_RIGHT,
+        TURN_HORIZONTAL,
+        TURN_VERTICAL,
+        JUMP,
+        CROUCH,
+        //Itemmanagement
+        SWITCH_TO_1,
+        SWITCH_TO_2,
+        SWITCH_TO_3,
+        NEXT_ITEM,
+        PREV_ITEM,
+        //Actions
+        DO_PRIMARY,
+        DO_SECONDARY,
+        DO_TERTIARY,
+        USE,
+        //Miscellaneous
+        OPEN_CONSOLE,
+        OPEN_MENU,
+        OPEN_INVENTORY,
+        OPEN_SKILLS,
+        OPEN_NETWORK,
+        OPEN_CHAT,
+        //Highest index of this enum
+        NUM_ACTIONS
+    };
+
+    class IInput
+    {
+    public:
+        virtual ~IInput() = default;
+
+        virtual void Update() = 0;
+    };
+}

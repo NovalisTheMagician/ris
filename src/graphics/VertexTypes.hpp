98bb309c6f6cd01499da5644001554623b738915
Patrik Körn <patrik.koern@gmail.com>
2020-11-17 03:34:25 +0100

    minor adjustments

--- src/graphics/VertexTypes.hpp
+++ src/graphics/VertexTypes.hpp
@@ -13,7 +13,8 @@ namespace RIS
             glm::vec2 position;
             glm::vec2 texCoords;
         };
-        constexpr Graphics::AttribFormat SpriteVertexFormat[2] = {{0, 2, GL_FLOAT, offsetof(VertexType::SpriteVertex, position), false, 0}, {1, 2, GL_FLOAT, offsetof(VertexType::SpriteVertex, texCoords), false, 0}};
+        constexpr Graphics::AttribFormat SpriteVertexFormat[] = {{0, 2, GL_FLOAT, offsetof(SpriteVertex, position), false, 0},
+                                                                 {1, 2, GL_FLOAT, offsetof(SpriteVertex, texCoords), false, 0}};
 
         struct ModelVertex
         {
@@ -23,15 +24,20 @@ namespace RIS
             glm::i16vec4 joints;
             glm::vec4 weights;
         };
-        constexpr Graphics::AttribFormat ModelVertexFormat[5] = {{0, 3, GL_FLOAT, 0, false, 0},
-                                                                 {1, 3, GL_FLOAT, 0, false, 1},
-                                                                 {2, 2, GL_FLOAT, 0, false, 2},
-                                                                 {3, 4, GL_UNSIGNED_SHORT, 0, false, 3},
-                                                                 {4, 4, GL_FLOAT, 0, false, 4}};
+        constexpr Graphics::AttribFormat ModelVertexFormat[] = {{0, 3, GL_FLOAT, 0, false, 0},
+                                                                {1, 3, GL_FLOAT, 0, false, 1},
+                                                                {2, 2, GL_FLOAT, 0, false, 2},
+                                                                {3, 4, GL_UNSIGNED_SHORT, 0, false, 3},
+                                                                {4, 4, GL_FLOAT, 0, false, 4}};
 
         struct MapVertex
         {
-            
+            glm::vec3 position;
+            glm::vec3 normal;
+            glm::vec2 texCoords;
         };
+        constexpr Graphics::AttribFormat MapVertexFormat[] = {{0, 3, GL_FLOAT, offsetof(MapVertex, position), false, 0},
+                                                              {1, 3, GL_FLOAT, offsetof(MapVertex, normal), false, 0},
+                                                              {2, 2, GL_FLOAT, offsetof(MapVertex, texCoords), false, 0}};
     }
 }
